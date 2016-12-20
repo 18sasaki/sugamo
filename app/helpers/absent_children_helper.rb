3f174f7aca76836612a25ecc498100980621673f
18sasaki <18sasaki@gmail.com>
2016-12-20 20:30:02 +0900

    class_roomのCRUD修正

--- app/helpers/absent_children_helper.rb
+++ app/helpers/absent_children_helper.rb
@@ -1,2 +1,9 @@
 module AbsentChildrenHelper
+  def absent_view_name(code_key)
+  	Code.find_by(code_type: 'absent', code_key: code_key).view_name
+  end
+
+  def reason_view_name(code_key)
+  	Code.find_by(code_type: 'reason', code_key: code_key).try(:view_name) || '理由不明'
+  end
 end
