876b7f775119899cafe39e307241ff579ca838ea
18sasaki <18sasaki@gmail.com>
2016-12-26 16:41:45 +0900

    園児の編集画面修正

--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -1,2 +1,7 @@
 module ChildrenHelper
+  def sex_list
+  	Constants::ALL_CODES['sex'].each_with_object([]) do |rea_hash, ret_list|
+  		ret_list << [rea_hash[1][:view_name], rea_hash[0]]
+  	end
+  end
 end
