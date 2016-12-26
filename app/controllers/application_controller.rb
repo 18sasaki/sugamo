ee8b79aed75c57fcaef962312b579f3fbeee2cac
18sasaki <18sasaki@gmail.com>
2016-12-26 18:56:35 +0900

    曜日が必ず現在の曜日になっていたバグ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def date_view(datetime)
-  	datetime.strftime("%m/%d（#{Constants::DOW[Time.now.wday]}）")
+  	datetime.strftime("%m/%d（#{Constants::DOW[datetime.wday]}）")
   end
 end
