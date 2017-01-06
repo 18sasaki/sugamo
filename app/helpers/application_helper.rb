d8f3043d88c043aa8769d6e5f9df6919316a48ed
18sasaki <18sasaki@gmail.com>
2017-01-06 18:53:37 +0900

    クラスへのリンクblockをhelperに切り出し

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def class_room_link_td(class_room_id, option = {})
+  	option.merge!(bgcolor: Constants::CR_HASH[class_room_id][:color])
+  	class_room_link_block('td', class_room_id, option)
+  end
+
+  def class_room_link_span(class_room_id, option = {})
+  	option.merge!(style: "background-color:#{Constants::CR_HASH[class_room_id][:color]};")
+  	class_room_link_block('span', class_room_id, option)
+  end
+
+  def class_room_link_block(tag, class_room_id, option = {})
+  	content_tag(tag, option) do
+  		link_to "/class_rooms/#{class_room_id}", style: 'text-decoration: none;' do
+  			Constants::CR_HASH[class_room_id][:name]
+  		end
+  	end
+  end
 end
